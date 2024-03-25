cea3977163066c68c23eaf6d941d25dd2e57e80c
Andrei Balint <toncara71@gmail.com>
2024-03-25 22:01:28 +0200

    second_task

--- first_task.py
+++ first_task.py
@@ -17,13 +17,3 @@ def get_birthdays_per_week(users):
         birthdays_per_week[birthday].append(name)
     for day,name in birthdays_per_week:
         print(f'{ day }')        
-
- 
-utilizatori = [
-    {"name": "Bill Gates", "birthday": datetime(1955, 10, 28)},
-    {"name": "Jan Koum", "birthday": datetime(1976, 2, 24)},
-    {"name": "Kim Kardashian", "birthday": datetime(1980, 10, 21)},
-    {"name": "Jill Valentine", "birthday": datetime(1974, 11, 30)},
-    {"name": "John Doe", "birthday": datetime(1990, 3, 12)} 
-]       
-get_birthdays_per_week(utilizatori)
